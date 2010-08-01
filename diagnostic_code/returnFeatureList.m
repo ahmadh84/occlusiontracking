9a50706049094e1b66d17d8b311bca0d0b760a77
Ahmad Humayun <ahmad.humyn@gmail.com>
2010-08-02 00:12:03 +0100

    Updated returnFeatureList to handle both files and objects

--- diagnostic_code/returnFeatureList.m
+++ diagnostic_code/returnFeatureList.m
@@ -1,46 +1,68 @@
-function [ feature_list scene_id unique_id ] = returnFeatureList( filepath )
-%RETURNFEATURELIST Takes a mat file for ClassifierOutputHandler or
-%   ComputeFeatureVectors and gets all the texts pertaining to all features
+function [ feature_list scene_id unique_id ] = returnFeatureList( filepath_or_obj, tooltip_format )
+%RETURNFEATURELIST Takes a mat file for or an object of 
+%   ClassifierOutputHandler or ComputeFeatureVectors and gets all the texts 
+%   pertaining to all features
 
-    assert(exist(filepath, 'file')==2, 'returnFeatureList:FileDoesntExist', 'The feature filepath given for listing doesn''t exist');
+    if ~exist('tooltip_format', 'var')
+        tooltip_format = 0;
+    end
+    
+    if isobject(filepath_or_obj)
+    % if it was an object
+        curr_obj = filepath_or_obj;
+        
+        if isa(curr_obj, 'ClassifierOutputHandler')
+            scene_id = curr_obj.scene_id;
+            unique_id = curr_obj.unique_id;
+        elseif isa(curr_obj, 'ComputeFeatureVectors')
+            scene_id = [];
+            unique_id = curr_obj.getUniqueID();
+        end
+        
+    elseif ischar(filepath_or_obj)
+    % if it was an class
+        assert(exist(filepath_or_obj, 'file')==2, 'returnFeatureList:FileDoesntExist', 'The feature filepath given for listing doesn''t exist');
 
-    % get the scene id and the unique id
-    tok = regexp(filepath, '(\d+)_(\w+)_((?:rffeatureimp)|(?:FV)).mat$', 'tokens');
-    assert(length(tok)==1 && length(tok{1})==3, 'Couldn''t decipher scene_id and unique_id from filename OR filename not supported');
-    scene_id = tok{1}{1};
-    unique_id = tok{1}{2};
+        % get the scene id and the unique id
+        tok = regexp(filepath_or_obj, '(\d+)_(\w+)_((?:rffeatureimp)|(?:FV)).mat$', 'tokens');
+        assert(length(tok)==1 && length(tok{1})==3, 'returnFeatureList:IncorrectFile', 'Couldn''t decipher scene_id and unique_id from filename OR filename not supported');
+        scene_id = tok{1}{1};
+        unique_id = tok{1}{2};
+
+        fprintf('--> Loading object from %s\n', filepath_or_obj);
+        s = load(filepath_or_obj);
+        temp = fields(s);
+        curr_obj = s.(temp{1});
+    else
+        error('returnFeatureList:Invalid input', 'Invalid input to returnFeatureList');
+    end
     
-    fprintf('--> Loading object from %s\n', filepath);
-    load(filepath);
-    
-    if strcmp(tok{1}{3}, 'rffeatureimp') > 0
+    if isa(curr_obj, 'ClassifierOutputHandler')
         % pull data from ClassifierOutputHandler
         
-        eval(['classifier_out = ' ClassifierOutputHandler.SAVE_OBJ_NAME ';']);
-        
         % validate feature length
-        ftr_depth = length(classifier_out.feature_importance);
-        no_ftr_types = length(classifier_out.feature_types);
-        assert(ftr_depth == sum(classifier_out.feature_depths), 'returnFeatureList:FeaturesMismatch', 'The feature data doesn''t match with feature_depths');
+        ftr_depth = length(curr_obj.feature_importance);
+        no_ftr_types = length(curr_obj.feature_types);
+        assert(ftr_depth == sum(curr_obj.feature_depths), 'returnFeatureList:FeaturesMismatch', 'The feature data doesn''t match with feature_depths');
         
         % get feature depths and cell_features
-        feature_depths = classifier_out.feature_depths;
-        cell_features = classifier_out.settings.cell_features;
-    else
+        feature_depths = curr_obj.feature_depths;
+        cell_features = curr_obj.settings.cell_features;
+    elseif isa(curr_obj, 'ComputeFeatureVectors')
         % pull data from ComputeFeatureVectors
         
-        eval(['comp_feature_vec = ' ComputeFeatureVectors.SAVE_OBJ_NAME ';']);
-
         % validate feature lengths
-        ftr_depth = size(comp_feature_vec.features,2);
-        no_ftr_types = length(comp_feature_vec.feature_types);
-        assert(ftr_depth == sum(comp_feature_vec.feature_depths), 'returnFeatureList:FeaturesMismatch', 'The feature data doesn''t match with feature_depths');
-        assert(no_ftr_types == length(comp_feature_vec.feature_depths), 'returnFeatureList:FeaturesMismatch', 'The no. of features doesn''t match with feature_depths');
-        assert(no_ftr_types == length(comp_feature_vec.cell_features), 'returnFeatureList:FeaturesMismatch', 'The no. of features doesn''t match with cell_features');
+        ftr_depth = size(curr_obj.features,2);
+        no_ftr_types = length(curr_obj.feature_types);
+        assert(ftr_depth == sum(curr_obj.feature_depths), 'returnFeatureList:FeaturesMismatch', 'The feature data doesn''t match with feature_depths');
+        assert(no_ftr_types == length(curr_obj.feature_depths), 'returnFeatureList:FeaturesMismatch', 'The no. of features doesn''t match with feature_depths');
+        assert(no_ftr_types == length(curr_obj.cell_features), 'returnFeatureList:FeaturesMismatch', 'The no. of features doesn''t match with cell_features');
 
         % get feature depths and cell_features
-        feature_depths = comp_feature_vec.feature_depths;
-        cell_features = comp_feature_vec.cell_features;
+        feature_depths = curr_obj.feature_depths;
+        cell_features = curr_obj.cell_features;
+    else
+        error('returnFeatureList:IncorrectFile', 'File given neither has ComputeFeatureVectors or ClassifierOutputHandler object');
     end
     
     
@@ -52,6 +74,16 @@ function [ feature_list scene_id unique_id ] = returnFeatureList( filepath )
         s = cell_features{feature_id}.returnFeatureList();
 
         assert(length(s)==feature_depths(feature_id), 'returnFeatureList:FeaturesMismatch', 'The no. of features returned by returnFeatureList() don''t match up');
+        
+        % if need plain string format
+        if ~tooltip_format
+            for i = 1:length(s)
+                temp = repmat({', '}, [1, length(s{i})*2-1]);
+                temp(1:2:length(s{i})*2) = s{i};
+                s{i} = horzcat(temp{:});
+            end
+        end
+        
         temp = sum(feature_depths(1:feature_id-1));
 
         feature_list(temp+1:temp+feature_depths(feature_id)) = s;
