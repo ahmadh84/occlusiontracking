ad099c49c63129edf92faffb012c3660fb85cfd6
Ahmad Humayun <ahmad.humyn@gmail.com>
2010-08-10 12:49:24 +0100

    main_code - Committing 2 new features (1) ReverseFlowConstancyFeature, (2) ReverseFlowAngleDiffFeature. Also made relevant changes in extraFVInfoStruct to accomodate the usage of reverse flow (its scalespace) for these new features

--- main_code/@ComputeTrainTestData/extraFVInfoStruct.m
+++ main_code/@ComputeTrainTestData/extraFVInfoStruct.m
@@ -6,6 +6,14 @@ function [ extra_info ] = extraFVInfoStruct( obj, im1, im2, calc_flows )
     extra_info.flow_scalespace.scale = obj.settings.uv_ss_info(2);
     extra_info.flow_scalespace.ss = cell(1, extra_info.flow_scalespace.no_scales);
     
+    % if calc_flows is computing reverse flow, then also store it in SS
+    if calc_flows.compute_reverse
+        extra_info.flow_scalespace_r.no_scales = obj.settings.uv_ss_info(1);
+        extra_info.flow_scalespace_r.scale = obj.settings.uv_ss_info(2);
+        extra_info.flow_scalespace_r.ss = cell(1, extra_info.flow_scalespace_r.no_scales);
+    end
+    
+    
     % iterate over  all candidate algos
     for flow_idx = 1:size(calc_flows.uv_flows,4)
         flow_scalespace_x = ComputeFeatureVectors.computeScaleSpace( calc_flows.uv_flows(:,:,1,flow_idx), ...
@@ -17,6 +25,22 @@ function [ extra_info ] = extraFVInfoStruct( obj, im1, im2, calc_flows )
         for scale_idx = 1:extra_info.flow_scalespace.no_scales
             temp = cat(3, flow_scalespace_x{scale_idx}, flow_scalespace_y{scale_idx});
             extra_info.flow_scalespace.ss{scale_idx} = cat(4, extra_info.flow_scalespace.ss{scale_idx}, temp);
+        end
+        
+        % if calc_flows is computing reverse flow, then also store it in SS
+        if calc_flows.compute_reverse
+            % do the same for reverse flow
+            
+            flow_scalespace_x = ComputeFeatureVectors.computeScaleSpace( calc_flows.uv_flows_reverse(:,:,1,flow_idx), ...
+                extra_info.flow_scalespace_r.no_scales, extra_info.flow_scalespace_r.scale );
+            flow_scalespace_y = ComputeFeatureVectors.computeScaleSpace( calc_flows.uv_flows_reverse(:,:,2,flow_idx), ...
+                extra_info.flow_scalespace_r.no_scales, extra_info.flow_scalespace_r.scale );
+
+            % iterate over all scales
+            for scale_idx = 1:extra_info.flow_scalespace_r.no_scales
+                temp = cat(3, flow_scalespace_x{scale_idx}, flow_scalespace_y{scale_idx});
+                extra_info.flow_scalespace_r.ss{scale_idx} = cat(4, extra_info.flow_scalespace_r.ss{scale_idx}, temp);
+            end
         end
     end
     
