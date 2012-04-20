5f032355afb2ac07b3bbe54c9b563291c041069d
Ahmad Humayun <ahmad.humyn@gmail.com>
2012-04-19 23:07:43 -0400

    Changing training directory name

--- main_code/testing_script.m
+++ main_code/testing_script.m
@@ -1,6 +1,6 @@
 function testing_script
     
-    training_dir = '/home/ahumayun/Desktop/AlgoSuitabilityDataset';
+    training_dir = '/home/ahumayun/Desktop/AlgoSuit+Middlebury_Dataset';
 
     seq_conflicts = {[2 3], [6 7 16], [11 12], [13 14], [18 19], [9 20 21 22 40:48 10 23 24 25], [26 27 28 29], [30:39], [49:50]};
     [ override_settings ] = create_override_settings( seq_conflicts, training_dir );
@@ -8,7 +8,7 @@ function testing_script
     
     %%%%%%%%%%%%%%%%%%%%%%%%%%%%%
     % Test middlebury sequences %
-    main_dir = '/home/ahumayun/Desktop/AlgoSuitabilityDataset';
+    main_dir = '/home/ahumayun/Desktop/AlgoSuit+Middlebury_Dataset';
     temp_out_dir = fullfile(out_dir, 'FINAL-ed_pb_pb_pc_st_stm_tg_av_lv_cs-max_rc_ra_fc_fc_fa_fn_sp');
 
     training_seq = [9 10 17 18 19 22 24 27 29 30 39 49 50];
